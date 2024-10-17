88af0b26538ec21db2a8755986226f75fe414ca5
Vaibhav <vaibhavjain0724@gmail.com>
2024-10-17 13:59:37 +0530

    new q

--- binarysearchprc.cpp
+++ binarysearchprc.cpp
@@ -49,4 +49,34 @@ int main(){
     return 0;
 
 }
+
+
+
+// ! Q-278 (First bad version)
+// give arr of len n and fnc isBAD, we gotta return the first bad element(every element after it is bad)
+// since we can divide or search space into 2 cont, we can use binary search
+
+//// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int s = 1;
+        int e = n;
+        int m = s+ (e-s)/2;
+        int ans = -1;
+        while(s<=e){
+            if(isBadVersion(m)){
+                ans = m;
+                e = m-1;
+            }
+            else{
+                s = m+1;
+            }
+            m = s+ (e-s)/2;
+        } 
+        return ans;
+    }
+};
     
